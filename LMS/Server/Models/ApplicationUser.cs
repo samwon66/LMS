8aef951d974c253b06afb73c65202f5c179f6343
Sammy Wong <sammywong528@outlook.com>
2023-12-29 10:29:28 +0100

    Init

--- LMS/Server/Models/ApplicationUser.cs
+++ LMS/Server/Models/ApplicationUser.cs
@@ -5,8 +5,8 @@ namespace LMS.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string FullName => FirstName + " " + LastName;
         
         public Guid CourseId { get; set; }
--- LMS/Server/Models/Domain/Module.cs
+++ LMS/Server/Models/Domain/Module.cs
@@ -9,13 +9,13 @@ namespace LMS.Server.Models.Domain
     public class Module
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public Guid CourseId { get; set; }
         public Course Course { get; set; }
-        public ICollection<Activity> Activities { get; set; }
+        public ICollection<Activity> Activities { get; set; } = new List<Activity>();
 
 
     }

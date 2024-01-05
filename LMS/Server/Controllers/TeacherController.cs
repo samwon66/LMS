8b4661f8509cbad607239b0902d8e017ce2fff4e
Erik Lagnefeldt <e.lagnefeldt@gmail.com>
2024-01-05 10:10:15 +0100

    Added teacheroverview and controller code.

---
+++ LMS/Server/Controllers/TeacherController.cs
@@ -0,0 +1,51 @@
+﻿using LMS.Server.Data;
+using LMS.Server.Models;
+using LMS.Server.Models.Domain;
+using LMS.Shared.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using LMS.Shared.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TeacherDTO>> GetUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userDTO = new TeacherDTO
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                FullName = user.FullName,
+                CourseId = user.CourseId
+            };
+
+            return Ok(userDTO);
+        }
+    }
+}
---
+++ LMS/Shared/Dtos/TeacherDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.Shared.Dtos
+{
+    public class TeacherDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public Guid CourseId { get; set; }
+    }
+}
